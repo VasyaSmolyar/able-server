5e108e81354f5f95a583f8cd58aefdd2efaf14ef
Vasily Smolyar <smolyar4@gmail.com>
2021-05-11 01:28:04 +0300

    Метод поиска по тикеру

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 
 const apiURL = 'https://api-invest.tinkoff.ru/openapi/sandbox'; // Для Production-окружения будет https://api-invest.tinkoff.ru/openapi
 const socketURL = 'wss://api-invest.tinkoff.ru/openapi/md/v1/md-openapi/ws';
-const secretToken = ''; // токен для сандбокса
+const secretToken = 't.hGeyci0o8BDXyi37Fs2GsqBFOXV6iBwJFChPHiIeSjlgO7sj9Xc99vbbRQJxb6rJIeIwAvKvO4xWPMXxhJty0A'; // токен для сандбокса
 const api = new OpenAPI({ apiURL, secretToken, socketURL });
 
 const app = express();
@@ -16,17 +16,36 @@ const port = 5000
 app.get('/candles/:ticker', async (req, res) => {
     try {
         const { figi } = await api.searchOne({ ticker: req.params.ticker });
+
         const payload = await api.candlesGet({
             figi,
             from: '2019-08-19T18:38:33.131642+03:00',
             to: '2020-08-19T18:38:33.131642+03:00',
             interval: 'day'
-        }); // Покупаем AAPL
+        });
+
         res.send(payload);
     } catch (err) {
         res.send(err);
     }
 })
+
+app.get('/list/:ticker', async (req, res) => {
+    try {
+        const stocks = await api.stocks();
+        const bonds = await api.bonds();
+        const etfs = await api.etfs();
+        const all = stocks.instruments.concat(bonds.instruments, etfs.instruments);
+
+        const payload = all.filter((figi) => {
+            return figi.ticker.startsWith(req.params.ticker.toUpperCase());
+        });
+        
+        res.send(payload);
+    } catch (err) {
+        res.send(err);
+    }
+});
 
 app.listen(port, () => {
     console.log(`Backend app listening at http://localhost:${port}`)
